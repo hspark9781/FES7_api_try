056167237bb567a07533aa231e36d42ed2ab8b24
hsprak9781 <hspark9781@gmail.com>
2023-10-06 13:39:15 +0900

    로그인 페이지 추가

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,11 @@
+import LoginPage from "./Components/LoginPage";
+
 function App() {
     // 로그인 기능 
     return (
         <div>
-            hello world
+            <LoginPage/>
         </div>
-    );
+    )
 }
 export default App;
