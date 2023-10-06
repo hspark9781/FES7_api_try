0228f0d4b159770ad998885fa0c9fa98202bcef9
hsprak9781 <hspark9781@gmail.com>
2023-10-06 15:15:04 +0900

    에러 핸들링

--- app/src/Components/LoginPage.jsx
+++ app/src/Components/LoginPage.jsx
@@ -16,21 +16,29 @@ function LoginPage() {
             }
         };
 
-        // fetch에서 POST, 명시 안하면 기본값이 GET
-        // 로그인해서 token 꺼내기
-        const res = await fetch(reqUrl, {
-            method: "POST", // 대문자로!
-            headers: {  
-                "Content-type" : "application/json"
-            },
-            body: JSON.stringify(loginData) // 문자열로 만들어줘야함
-        })
-        const json = await res.json();
-        console.log(json);
-        const token = json.user.token;
-        console.log(token);
-        // 로컬스토리지에 토큰 저장하기
-        localStorage.setItem("token", token);
+        try {
+            // fetch에서 POST, 명시 안하면 기본값이 GET
+            // 로그인해서 token 꺼내기
+            const res = await fetch(reqUrl, {
+                method: "POST", // 대문자로!
+                headers: {  
+                    "Content-type" : "application/json"
+                },
+                body: JSON.stringify(loginData) // 문자열로 만들어줘야함
+            })
+            const json = await res.json();
+            console.log(json);
+            // 객체에 user가 없는 경우 == 로그인 실패 했을 때
+            // if(!json.user) {
+            //     return
+            // }
+            const token = json.user.token;
+            console.log(token);
+            // 로컬스토리지에 토큰 저장하기
+            localStorage.setItem("token", token);
+        } catch (error) {
+            alert("로그인에 실패했습니다.");
+        }
     }
 
     const inputEmail = (e) => {
