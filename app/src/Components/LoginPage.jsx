d439efb1d5f9d1286082f6be4f60fbf0cc02b49d
hsprak9781 <hspark9781@gmail.com>
2023-10-06 14:41:26 +0900

    -로그인 기능 완성

--- app/src/Components/LoginPage.jsx
+++ app/src/Components/LoginPage.jsx
@@ -1,14 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function LoginPage() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const login = async (email, password) => {
+        const baseUrl = "https://api.mandarin.weniv.co.kr";
+        const reqPath = "/user/login";
+        const reqUrl = baseUrl + reqPath;
+
+        const loginData = {
+            "user" : {
+                "email" : email,
+                "password" : password
+            }
+        };
+
+        // fetch에서 POST, 명시 안하면 기본값이 GET
+        // 로그인해서 token 꺼내기
+        const res = await fetch(reqUrl, {
+            method: "POST", // 대문자로!
+            headers: {  
+                "Content-type" : "application/json"
+            },
+            body: JSON.stringify(loginData) // 문자열로 만들어줘야함
+        })
+        const json = await res.json();
+        console.log(json);
+        const token = json.user.token;
+        console.log(token);
+        // 로컬스토리지에 토큰 저장하기
+        localStorage.setItem("token", token);
+    }
+
+    const inputEmail = (e) => {
+        setEmail(e.target.value);
+    }
+
+    const inputPassword = (e) => {
+        setPassword(e.target.value);
+    }
+
+    const submitLogin = (e) => {
+        e.preventDefault(); 
+        login(email, password);
+    }
+
     return (
         <>
             <h1>로그인</h1>
             <section>
                 <h2>이메일, 비밀번호 입력하는 곳</h2>
-                <form>
-                    <input type="text" placeholder='이메일 입력'/>
-                    <input type="text" placeholder='비밀번호 입력'/>
+                <form onSubmit={submitLogin}>
+                    <input type="text" placeholder="이메일 입력" onChange={inputEmail} value={email}/>
+                    <input type="text" placeholder="비밀번호 입력" onChange={inputPassword} value={password}/>
                     <button>로그인</button>
                 </form>
             </section>
