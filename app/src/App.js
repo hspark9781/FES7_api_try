95df7d405bd6937f1a6f3457f7b942ffeae28b25
hsprak9781 <hspark9781@gmail.com>
2023-10-06 17:59:49 +0900

    내 정보 불러오기 기능,  화면에 추가

--- app/src/App.js
+++ app/src/App.js
@@ -4,13 +4,28 @@ import { useState } from "react";
 
 function App() {
     const [page, setPage] = useState(true);
+    const [info, setInfo] = useState("");
     const handlePage = () => {
         setPage((prevPage) => {
             return !prevPage;
         })
     }
+    const  getMyinfo = async () => {
+        const token = localStorage.getItem("token");
+        const reqUrl = "https://api.mandarin.weniv.co.kr/user/myinfo";
+        const res = await fetch(reqUrl, {
+            headers: {
+                Authorization : `Bearer ${token}`
+            }
+        })
+        const json = await res.json();
+        console.log(json);
+        setInfo(JSON.stringify(json));
+    }
     return (
         <div>
+            <button type="button" onClick={getMyinfo}>내 정보 불러오기</button>
+            {info}
             {page ? <LoginPage handlePage={handlePage}/> : <JoinPage handlePage={handlePage}/>}
         </div>
     )
